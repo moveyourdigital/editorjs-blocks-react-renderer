8da3a71651896ccc98d0ab46bb790daad5fef5b9
Vitor Carvalho <lightningspirit@gmail.com>
2021-10-19 12:56:16 +0100

    Added prettier fixes

--- src/renderers/quote/index.test.tsx
+++ src/renderers/quote/index.test.tsx
@@ -5,8 +5,7 @@ import Quote, { QuoteBlockData } from '.';
 describe('<Quote />', () => {
   describe('when receives a Quote block', () => {
     const data: QuoteBlockData = {
-      text:
-        '<i>Esse ea consectetur est fugiat ut dolor pariatur ex voluptate ad Lorem Lorem sit sunt.</i>\n\n<i>Eiusmod voluptate <b>ullamco</b> laborum minim elit.</i><i><br></i>Cillum ullamco fugiat tempor dolore enim.\nExcepteur exercitation amet aliqua proident labore qui sint do dolore sint nulla aute.</i>',
+      text: '<i>Esse ea consectetur est fugiat ut dolor pariatur ex voluptate ad Lorem Lorem sit sunt.</i>\n\n<i>Eiusmod voluptate <b>ullamco</b> laborum minim elit.</i><i><br></i>Cillum ullamco fugiat tempor dolore enim.\nExcepteur exercitation amet aliqua proident labore qui sint do dolore sint nulla aute.</i>',
       caption: 'Carpe Diem, <cite>Lorem ipusm dolor</cite>.',
       alignment: 'center',
     };
