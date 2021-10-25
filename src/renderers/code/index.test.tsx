061c9cea55570741e52faba1822ca60ce5127eed
Vitor Carvalho <lightningspirit@gmail.com>
2021-10-25 13:45:55 +0100

    Added more tests to code block

--- src/renderers/code/index.test.tsx
+++ src/renderers/code/index.test.tsx
@@ -13,4 +13,25 @@ describe('<Code />', () => {
       expect(create(<Code data={data} />).toJSON()).toMatchSnapshot();
     });
   });
+
+  describe('when receives a code block with className', () => {
+    const data: CodeBlockData = {
+      code: 'body {\n font-size: 14px;\n line-height: 16px;\n}',
+      lang: 'text/html',
+    };
+
+    it('renders a <pre> tag with <code className>', () => {
+      expect(create(<Code data={data} className={"code-block"} />).toJSON()).toMatchSnapshot();
+    });
+  });
+
+  describe('when receives a code block without lang', () => {
+    const data: CodeBlockData = {
+      code: 'body {\n font-size: 14px;\n line-height: 16px;\n}',
+    };
+
+    it('renders a <pre> tag with <code> without lang property', () => {
+      expect(create(<Code data={data} />).toJSON()).toMatchSnapshot();
+    });
+  });
 });
