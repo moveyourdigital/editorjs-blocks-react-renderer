7c737c1fa35571c9f662f83ae4faa582a5606b05
Vitor Carvalho <lightningspirit@gmail.com>
2021-10-27 11:02:13 +0100

    Support NestedList
    Editor.js now has a NestedList tool which (unfortunately) uses the
    same block name "list" as the simple List block tool.
    
    As such, this adds support to nested ul and ol tags following
    the output showed in
    https://github.com/editor-js/nested-list#output-data
    
    Here's an example of such output data:
    ```json
    {
        "type" : "list",
        "data" : {
            "style" : "unordered",
            "items" : [
                {
                  "content": "Apples",
                  "items": [
                    {
                      "content": "Red",
                      "items": []
                    },
                    {
                      "content": "Green",
                      "items": []
                    },
                  ]
                },
                {
                  "content": "Bananas",
                  "items": [
                    {
                      "content": "Yellow",
                      "items": []
                    },
                  ]
                },
            ]
        }
    }
    ```
    
    Closes #30

--- src/renderers/list/index.test.tsx
+++ src/renderers/list/index.test.tsx
@@ -3,9 +3,13 @@ import { create } from 'react-test-renderer';
 import List, { ListBlockData } from '.';
 
 describe('<List />', () => {
-  describe('when receives a list unordered block', () => {
+  describe.each([
+    ['unordered', '<ul>'],
+    ['ordered', '<ol>'],
+  ])('when receives a list %p block', (style, tag) => {
     const data: ListBlockData = {
-      style: 'unordered',
+      // @ts-expect-error
+      style,
       items: [
         'It is a block-styled editor',
         'It returns clean <b>data output</b> in JSON',
@@ -13,23 +17,60 @@ describe('<List />', () => {
       ],
     };
 
-    it('renders a <ul> block', () => {
+    it(`renders a ${tag} block`, () => {
       expect(create(<List data={data} />).toJSON()).toMatchSnapshot();
     });
   });
 
-  describe('when receives a list ordered block', () => {
+  describe.each([
+    ['unordered', '<ul>'],
+    ['ordered', '<ol>'],
+  ])('when receives a nested list %p block', (style, tag) => {
     const data: ListBlockData = {
-      style: 'ordered',
+      // @ts-expect-error
+      style,
       items: [
-        'It is a block-styled editor',
-        'It returns clean data output in JSON',
-        'Designed to be extendable and pluggable with a simple API',
+        {
+          content: 'It is a block-styled editor',
+          items: [],
+        },
+        {
+          content: 'It returns clean data output in JSON',
+          items: [
+            {
+              content: 'Designed to be extendable and pluggable with a simple API',
+              items: [
+                {
+                  content: 'Red',
+                  items: [],
+                },
+                {
+                  content: 'Green',
+                  items: [],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          content: 'It returns clean data output in JSON',
+          items: [],
+        },
+        {
+          content: 'Designed to be extendable and pluggable with a simple API',
+          items: [],
+        },
       ],
     };
 
-    it('renders a <ol> block', () => {
+    it(`renders a ${tag} block`, () => {
       expect(create(<List data={data} />).toJSON()).toMatchSnapshot();
+    });
+
+    describe('and when className is provided', () => {
+      it.each(['list', 'list-ul px-3 py-2'])(`renders className to all ${tag} blocks`, (className) => {
+        expect(create(<List data={data} className={className} />).toJSON()).toMatchSnapshot();
+      });
     });
   });
 });
--- src/renderers/list/index.tsx
+++ src/renderers/list/index.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { FC, PropsWithChildren } from 'react';
 import HTMLReactParser from 'html-react-parser';
 import { RenderFn } from '../..';
 
 export interface ListBlockData {
   style: 'ordered' | 'unordered';
-  items: string[];
+  items: NestedListItem[];
 }
+
+export type NestedListItem =
+  | {
+      content: string;
+      items: NestedListItem[];
+    }
+  | string;
+
+const Bullet: FC<PropsWithChildren<{}>> = ({ children }) => <li>{children}</li>;
+
+const Group: FC<{
+  Tag: keyof JSX.IntrinsicElements;
+  items: NestedListItem[];
+  className?: string;
+}> = ({ Tag, items, ...props }) => (
+  <Tag {...props}>
+    {items.map((item, i) => (
+      <Bullet key={i}>
+        {typeof item === 'string' ? (
+          HTMLReactParser(item)
+        ) : (
+          <>
+            {HTMLReactParser(item?.content)}
+            {item?.items?.length > 0 && <Group Tag={Tag} items={item.items} {...props} />}
+          </>
+        )}
+      </Bullet>
+    ))}
+  </Tag>
+);
 
 const List: RenderFn<ListBlockData> = ({ data, className = '' }) => {
   const props: {
@@ -17,13 +47,7 @@ const List: RenderFn<ListBlockData> = ({ data, className = '' }) => {
   }
 
   const Tag = (data?.style === 'ordered' ? `ol` : `ul`) as keyof JSX.IntrinsicElements;
-  return (
-    <Tag {...props}>
-      {data?.items.map((item, i) => (
-        <li key={i}>{HTMLReactParser(item)}</li>
-      ))}
-    </Tag>
-  );
+  return data && <Group Tag={Tag} items={data.items} {...props} />;
 };
 
 export default List;
