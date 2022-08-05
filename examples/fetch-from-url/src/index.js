61e497e6862af3b989d744b20494555532481062
Vitor Carvalho <lightningspirit@gmail.com>
2022-08-05 16:15:22 +0100

    Add a simple fetch example
    This example uses the native `fetch` to get a JSON file that
    has the editor.js format and renders in React.

---
+++ examples/fetch-from-url/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Fetch from './Fetch';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Fetch />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
